4778c7350856e9baa86f1bff65e6549ab281bbac
nam812 <2022uch1084@mnit.ac.in>
2025-03-24 22:32:07 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+// src/App.js
+import styles from './App.module.css';
+import { Navbar } from './components/Navbar/Navbar';
+import { Hero } from './components/Hero/Hero';
+import { About } from './components/About/About';
+import { Experience } from './components/Experience/Experience';
+import { Projects } from './components/Projects/Projects';
+import { Contact } from './components/Contact/Contact';
+import ResumeChatbot from './components/Chatbot/ResumeChatbot';
+
+function App() {
+  return (
+    <div className={styles.App}>
+      <Navbar />
+      <Hero />
+      <About />
+      <Experience />
+      <Projects />
+      <Contact />
+      <ResumeChatbot /> {/* Adding the chatbot component */}
+    </div>
+  );
+}
+
+export default App;
